e66391f5122efb4b953109d0dabc5c566b9dc44e
Kash Karimi <kash.karimi@gmail.com>
2017-04-01 22:20:18 +0100

    default required vars

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "DO_TOKEN"                 {}
+
+variable "droplet_name"             {}
+variable "droplet_region"           {}
+variable "droplet_size"             {}
+
+variable "public_key"               {}
+variable "private_key"              {}
+
+variable "DRONE_GITHUB_CLIENT"      {}
+variable "DRONE_GITHUB_SECRET"      {}
+variable "DRONE_SECRET"             {}
